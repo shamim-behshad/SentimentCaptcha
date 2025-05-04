6a9ad9db8243cae1df6868d5fafe8c0d74ffa412
shamim-behshad <shamim.behshad@gmail.com>
2025-05-04 14:19:07 +0330

    fix: fixing profile component

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,36 @@
-function profile() {
-    return (
-      <p>Profile</p>
-    )
-  }
-  
-  export default profile;
+
+
+export default function ProfilePage() {
+  return (
+    <main>
+      <div>
+      
+        <div className="mt-6 text-center">
+          <p className="text-gray-700">
+            Passionate about building beautiful, user-friendly web apps with modern technologies like React, Tailwind CSS, and TypeScript.
+          </p>
+        </div>
+
+
+        <div >
+          <a
+            href=""
+            className="text-gray-600 hover:text-black transition"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          </a>
+
+          <a
+            href="https://linkedin.com/in/janedoe"
+            className="text-blue-700 hover:text-blue-900 transition"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+
+          </a>
+        </div>
+      </div>
+    </main>
+  );
+}
